41d6e493c4cacf4394bc56e291077a9a59be86ad
WafiWadud <wafinashwan@gmail.com>
2024-05-31 16:03:43 +0600

    i dunno

--- betterui.py
+++ betterui.py
@@ -11,9 +11,10 @@ def gzip_directory() -> None:
     shutil.make_archive(output_filename, 'gztar', dir_to_zip)
     result.set_text(f"Directory {dir_to_zip} has been gzipped successfully!")
 
-result = ui.label(text='')
-directory = ui.input(label='Enter directory to gzip')
-output = ui.input(label='Enter output filename')
-ui.button(text='Gzip Directory', on_click=gzip_directory)
+with ui.grid().classes("mx-auto"):
+    result = ui.label(text='')
+    directory = ui.input(label='Enter directory to gzip')
+    output = ui.input(label='Enter output filename')
+    ui.button(text='Gzip Directory', on_click=gzip_directory)
 
-ui.run(native=True)
+ui.run(native=True, reload=False)
